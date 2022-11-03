e9d66f42db0ca31a88fb8a0ba0778280accbf063
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-11-03 15:56:11 -0300

    cria componente header e adicina ao search, album e favorites

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header data-testid="header-component">
+        <h1>Header</h1>
+      </header>
+    );
+  }
+}
