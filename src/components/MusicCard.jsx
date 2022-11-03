65d3aa0e2499baf8a122f243e6f98e15e8223352
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-11-03 19:28:28 -0300

    adiciona lista de músicas ao componente album

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Props from 'prop-types';
+
+export default class MusicCard extends Component {
+  render() {
+    const { trackName, previewUrl } = this.props;
+    return (
+      <div>
+        <span>{trackName}</span>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          <code>audio</code>
+          .
+        </audio>
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  trackName: Props.string,
+  previewUrl: Props.string,
+}.isRequired;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
+import Props from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 export default class Album extends Component {
+  state = {
+    title: '',
+    musicas: [],
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const { id } = match.params;
+    const [titulo, ...music] = await getMusics(id);
+    this.setState({
+      title: titulo,
+      musicas: music,
+    });
+  }
+
   render() {
+    const { musicas, title } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        <h2 data-testid="artist-name">{title.artistName}</h2>
+        <h3 data-testid="album-name">{title.collectionName}</h3>
+        {musicas.map((musica, index) => (<MusicCard
+          key={ musica.trackId + index }
+          trackName={ musica.trackName }
+          previewUrl={ musica.previewUrl }
+        />))}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: Props.shape({
+    params: Props.shape({
+      id: Props.number,
+    }),
+  }),
+}.isRequired;
